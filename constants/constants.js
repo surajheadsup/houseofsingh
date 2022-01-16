a8c6454b99ab920776892543255448f4f65fee9d
Suraj Kumar <surajkumarmca15@gmail.com>
2022-01-16 23:08:32 +0530

    SSL Added into server 2

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const BaseUrl = 'https://db.houseofsingh.com/api/'
-export const GraphQLUrl = 'https://server.houseofsingh.com'
+export const GraphQLUrl = 'https://server.houseofsingh.com/'
 export const WordPressRestApiBaseURL = 'https://server.houseofsingh.com/wp-json/wp/v2/posts/'
 // export const BaseUrl = 'http://localhost:8000/api/'
 export const UploadUrl = 'https://db.houseofsingh.com//uploads/timelines/'
