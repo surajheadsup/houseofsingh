de81cc07879282b412be19fb2fe6e4ef15d28955
Suraj Kumar <surajkumarmca15@gmail.com>
2022-01-23 19:18:14 +0530

    Navbar 2 issue fixed

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Label from '../components/Label';
 import styles from '../styles/Contact.module.css'
-import Navbar2 from '../components/Navbar2'
+import Navbar2 from "../components/Navbar2";
 
 const Contact = () => {
     return ( <>
