b0ead5c2e0830c7c2e42cf0276b4ae04f2ac3dec
surajheadsup <90753070+surajheadsup@users.noreply.github.com>
2022-01-27 23:12:31 +0530

    animation time chnage for logo

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ axios.defaults.baseURL = "http://localhost:8000/";
 
  function App({ Component, pageProps }) {
   const [loading, setLoading] = React.useState(false);
-  const [timer, setTimer] = useState(1800);
+  const [timer, setTimer] = useState(2400);
 
   React.useEffect(() => {
 
